158be4b21c29a1806714a4c0b6795cdee5ae29f2
Isac Galdino Ferreira <sougoku705@gmail.com>
2026-09-17 21:39:18 +0000

    ajustando a forma como a classe é chamada e o começo da criação de uma classe produto

--- teste-virtualshop.py
+++ teste-virtualshop.py
@@ -1,5 +1,5 @@
-breakline = print(26*'=')
-class Shop:
+
+class Client:
     def __init__(self, emitent, destin, note_ident, email):
         self.emitent = emitent
         self.destin = destin
@@ -17,19 +17,17 @@ class Shop:
         while(True):
             menu = int(input("O que você deseja alterar?\n1 - Alterar emitente\n2 - Alterar destinatário\n3 - Alterar email\n4 - Sair\n--> "))
             if menu == 1:
-                print(breakline)
                 new_emitent = input("informe o nome do novo emitente:\n--> ")
                 self.emitent = new_emitent
                 print("Emitente alterado com sucesso!")
             elif menu == 2:
-                print(breakline)
                 new_destin = input("Informe o novo destinatário:\n--> ")
                 self.destin = new_destin
                 print("Destinatário alterado com sucesso")
             elif menu == 3:
-                print(breakline)
                 new_email = input("Informe o novo e-mail:\n--> ")
                 if "@" not in new_email:
+
                     print("Informe um e-mail válido")
                     continue
                 else:
@@ -37,20 +35,14 @@ class Shop:
                     print("E-mail atlerado com sucesso")
             elif menu == 4:
                 return "saindo..."
-                break
             else:
                 continue
-class Show_results(Shop):
-    def results():
-        cliente1 = Shop("Galdino","adiel",10,"isac.galdino@gmail.com")
-        print(cliente1.get_info())
+class Product:
+    def __init__(id, name, price, stock)
+    
+# cliente1 = Shop("Galdino", "Adiel", 10, "isac.galdino@gmail.com")
+# print(cliente1.get_info())
+# cliente1.set_info()
+# print(cliente1.get_info())
 
-class Change_results(Shop):
-    def change():
-        cliente1 = Shop("Galdino","adiel",10,"isac.galdino@gmail.com")
-        print(cliente1.set_info())
-
-Show_results.results()
-Change_results.change()
-Show_results.results()
 
